43edd69c314dc32e34785f3e7e1c511c35e9c3ed
Victor M Oliveira <rhapsodyv@gmail.com>
2015-11-24 23:01:02 -0200

    soh pra deixar mais bonito e colorido :)

---
+++ lotery_system.rb
@@ -0,0 +1,116 @@
+class LoterySystem
+  attr_reader :n, :k, :g
+  attr_reader :possible_nk, :possible_ng, :possible_kg
+  attr_reader :combs_nk, :combs_ng, :combs_kg
+
+  def initialize(n, k, g)
+    @n = n
+    @k = k
+    @g = g
+
+    @possible_nk = Combinatorics::Choose.C(n, k)
+    @possible_ng = Combinatorics::Choose.C(n, g)
+    @possible_kg = Combinatorics::Choose.C(k, g)
+
+    @combs_nk = (0..n-1).to_a.choose(k).map(&:to_a)
+    @combs_ng = (0..n-1).to_a.choose(g).map(&:to_a)
+    @combs_kg = (0..k-1).to_a.choose(g).map(&:to_a)
+
+    @pad = (@possible_ng - 1).to_s.size + 1
+    gen_all_indices_nk
+  end
+
+  def print_indices_nk
+    i = 0
+
+    @all_indices_nk.map do |comb|
+      comb2 = Array.new(@possible_ng, ' ' * @pad)
+      comb.each do |c|
+        comb2[c] = c.to_s.rjust(@pad)
+      end
+
+      print "#{i.to_s.rjust(2)}: ["
+      comb2.each_with_index do |c, idx|
+        print c
+      end
+      print "]\n"
+      #print "#{i.to_s.rjust(2)}: #{comb2.inspect}\n"
+      i += 1
+    end
+  end
+
+  def print_indices_nk_with_formation
+    left =   LoterySystem.new(n - 1, k - 1, g - 1)
+    right =  LoterySystem.new(n - 1, k - 1, g    )
+    bottom = LoterySystem.new(n - 1, k    , g    )
+
+    i = 0
+    @all_indices_nk.map do |comb|
+      comb2 = Array.new(@possible_ng, ' ' * @pad)
+      comb.each do |c|
+        comb2[c] = c.to_s.rjust(@pad)
+      end
+
+      print "#{i.to_s.rjust(2)}: ["
+      comb2.each_with_index do |c, idx|
+        color = :green
+        if idx >= left.possible_ng
+          color = :red
+        end
+        if i >= left.possible_nk
+          color = :blue
+        end
+        print c.colorize(color)
+      end
+      print "]\n"
+
+      i += 1
+    end
+
+    puts "Esquerda: #{left.n} #{left.k} #{left.g}"
+    puts "Direita : #{right.n} #{right.k} #{right.g}"
+    puts "Em baixo: #{bottom.n} #{bottom.k} #{bottom.g}"
+  end
+
+  def gen_all_indices_nk
+    @all_indices_nk = @combs_nk.map do |nk|
+      #puts "nk: #{nk}"
+      indices_nk = @combs_kg.map do |kg|
+        #puts "kg: #{kg}"
+        comb = kg.map do |c|
+          nk[c]
+        end
+        comb_index(@n, @g, comb.clone)
+      end
+      indices_nk
+    end
+  end
+
+  def sum_col(n, k, x)
+    return 0 if n == 0 || k == 0 || x == 0
+    c = Combinatorics::Choose.C(n - 1, k - 1)
+    # esse caco nao ta calculando C direito... enfim
+    c = 1 if c == 0
+    return c + sum_col(n - 1, k, x - 1)
+  end
+
+  def comb_index2(n, k, comb, l)
+    return 0 if comb.size == 0
+    x = comb.shift
+    x2 = x - l - 1
+    n2 = n - x2 - 1
+    k2 = k - 1
+    s = sum_col(n, k, x2)
+    return s + comb_index2(n2, k2, comb, x)
+  end
+
+  # Isso aqui eh meio magico e só traduzi de haskell pra ruby..
+  # O que ele faz eh pegar uma combinacao e procurar o indice dela numa tabela de combinacoes
+  def comb_index(n, k, comb)
+    return comb_index2(n, k, comb, -1)
+  end
+
+  def print_comb(comb)
+
+  end
+end
--- lottery-system.rb
+++ lottery-system.rb
@@ -1,4 +1,9 @@
-require 'combinatorics/choose'
+require 'rubygems'
+require 'bundler/setup'
+#require 'combinatorics/choose'
+Bundler.require(:default)
+
+require './lotery_system.rb'
 
 if ARGV.size != 3
   puts "use: #{__FILE__} total_numeros tamanho_sorteio tamanho_objetivo"
@@ -9,56 +14,29 @@ n = ARGV[0].to_i
 k = ARGV[1].to_i
 g = ARGV[2].to_i
 
-possible_nk = Combinatorics::Choose.C(n, k)
-possible_ng = Combinatorics::Choose.C(n, g)
-possible_kg = Combinatorics::Choose.C(k, g)
+lotery_system = LoterySystem.new(n, k, g)
 
-puts "Total de cartoes possiveis de #{n} numeros e tamanho #{k}: #{possible_nk}"
+puts "Total de cartoes possiveis de #{n} numeros e tamanho #{k}: #{lotery_system.possible_nk}"
 puts "Vamos ver como sao esses cartoes possiveis..."
 STDIN.gets
-combs_nk = (0..n-1).to_a.choose(k).map(&:to_a)
-combs_nk.each do |comb|
+lotery_system.combs_nk.each do |comb|
   p comb
 end
 
 puts
-puts "Total de combinacoes possiveis de #{n} numeros e tamanho #{g}: #{possible_ng}"
+puts "Total de combinacoes possiveis de #{n} numeros e tamanho #{g}: #{lotery_system.possible_ng}"
 puts "Vamos ver como sao essas combinacoes..."
 STDIN.gets
-combs_ng = (0..n-1).to_a.choose(g).map(&:to_a)
-combs_ng.each do |comb|
+lotery_system.combs_ng.each do |comb|
   p comb
 end
 
-# Isso aqui eh meio magico e só traduzi de haskell pra ruby..
-# O que ele faz eh pegar uma combinacao e procurar o indice dela numa tabela de combinacoes
-def comb_index(n, k, comb)
-  def sum_col(n, k, x)
-    return 0 if n == 0 || k == 0 || x == 0
-    c = Combinatorics::Choose.C(n - 1, k - 1)
-    # esse caco nao ta calculando C direito... enfim
-    c = 1 if c == 0
-    return c + sum_col(n - 1, k, x - 1)
-  end
-  def comb_index2(n, k, comb, l)
-    return 0 if comb.size == 0
-    x = comb.shift
-    x2 = x - l - 1
-    n2 = n - x2 - 1
-    k2 = k - 1
-    s = sum_col(n, k, x2)
-    return s + comb_index2(n2, k2, comb, x)
-  end
-  return comb_index2(n, k, comb, -1)
-end
-
 puts
-puts "Mas cada cartao de #{k} numeros gera #{possible_kg} combinacoes possiveis de #{g} numeros"
+puts "Mas cada cartao de #{k} numeros gera #{lotery_system.possible_kg} combinacoes possiveis de #{g} numeros"
 puts "Vamos ver como sao essas combinacoes..."
 STDIN.gets
-combs_kg = (0..k-1).to_a.choose(g).map(&:to_a)
-indices = combs_kg.map do |comb|
-  idx = comb_index(n, g, comb.clone)
+indices = lotery_system.combs_kg.map do |comb|
+  idx = lotery_system.comb_index(n, g, comb.clone)
   print "#{comb.inspect} => indice: #{idx}\n"
   idx
 end
@@ -67,32 +45,10 @@ puts "Indices: #{indices.inspect}"
 puts
 puts "E se pegarmos todos os cartoes e olharmos para cada combinacao menor que cada um deles gera e dai pegar os indices delas?"
 STDIN.gets
-# agora tem q pegar todas combinacoes possiveis e ver qual cada um gera das combinacoes menores
-indices = combs_nk.map do |ng|
-  #puts "ng: #{ng}"
-  indices_ng = combs_kg.map do |kg|
-    #puts "kg: #{kg}"
-    comb = kg.map do |c|
-      ng[c]
-    end
-    comb_index(n, g, comb.clone)
-  end
-  p indices_ng
-  indices_ng
-end
-#p indices
+lotery_system.print_indices_nk
 
-puts
-puts "E se nos colocassemos cada numero na sua propria coluna?"
+puts "\n\n"
+puts "Agora vem a loucura e a beleza da matematica... E se eu te disser que essa tabela acima eh formada por outros sistemas de loterias??"
 STDIN.gets
-i = 0
-indices.map do |comb|
-  comb2 = Array.new(possible_ng, '  ')
-  comb.each do |c|
-    comb2[c] = c.to_s.rjust(2)
-  end
-  print "#{i.to_s.rjust(2)}: #{comb2.inspect}\n"
-  i += 1
-end
 
-
+lotery_system.print_indices_nk_with_formation
